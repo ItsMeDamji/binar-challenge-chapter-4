f8dd8c712982be39d9c650a62282560c682242cf
Damar Aji <damarajim7@gmail.com>
2022-07-24 21:36:36 +0700

    done game logic and refresh function, why's my disable function not working?

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-const playerBtns = document.querySelectorAll('[data-selection]')
+// const batuPlayer = document.querySelector(".batuPlayer");
+// const guntingPlayer = document.querySelector(".guntingPlayer");
+// const kertasPlayer = document.querySelector(".kertasPlayer");
+const gameImg = document.querySelector(".gameimage");
 const btnBatu = document.querySelector(".batu");
 const btnKertas = document.querySelector(".kertas");
 const btnGunting = document.querySelector(".gunting");
@@ -16,42 +19,80 @@ const draw = document.querySelector(".draw");
 playerWin.classList.add("hide");
 comWin.classList.add("hide");
 draw.classList.add("hide");
-
-// determining value of each button click
-const playerChoice = playerBtns.forEach(playerBtn => {
-    playerBtn.addEventListener('click', e => {
-        const selectionName = playerBtn.dataset.selection
-        makeSelection(selectionName)
-    })
-})
-
-function makeSelection(selection) {
-    console.log(selection)
-}
-
-// random generate value of com pick
-const computerOptions = ["batu", "gunting", "kertas",];
-
-playerBtns.forEach(playerBtn => {
-    playerBtn.addEventListener('click', function() {
-        const computerNumber = Math.floor(Math.random() * 3);
-        const computerChoice = computerOptions[computerNumber];
-        console.log(computerChoice)
-    })
-})
-
-// VS and option bg
-const playerButtons = [btnBatu, btnKertas, btnGunting,];
-
-playerButtons.forEach(playerButton => {
-    playerButton.addEventListener ('click', function() {
-        versusAnc.classList.add("hide");
-        playerButton.classList.add("changeColor");
-    })
-})
-
+btnBatu.classList.add("disable")
 // game logic
-const gameResult = (playerChoice,computerChoice) => {
-    if(playerChoice === computerChoice){
+// btn batu
+btnBatu.addEventListener("click", function(){
+    // whys my disable not working?
+    // batuPlayer.disabled = true;
+    // guntingPlayer.disabled = true;
+    // kertasPlayer.disabled = true;
+    btnBatu.classList.add("changeColor");
+    versusAnc.classList.add("hide");
+    const comPick = Math.floor(Math.random() * 3)+1;
+    console.log(comPick)
+    if (comPick === 1) {
+        btnBatuCom.classList.add("changeColor");
         draw.classList.remove("hide");
-    }}
+    } else if (comPick === 2) {
+        btnKertasCom.classList.add("changeColor");
+        comWin.classList.remove("hide");
+    } else if (comPick === 3) {
+        btnGuntingCom.classList.add("changeColor");
+        playerWin.classList.remove("hide");
+    }
+});
+// btn gunting
+btnGunting.addEventListener("click", function(){
+    btnBatu.disabled = "true";
+    btnKertas.disabled = "true";
+    btnGunting.disabled = "true";
+    btnGunting.classList.add("changeColor");
+    versusAnc.classList.add("hide");
+    const comPick = Math.floor(Math.random() * 3)+1;
+    console.log(comPick)
+    if (comPick === 1) {
+        btnBatuCom.classList.add("changeColor");
+        comWin.classList.remove("hide");
+    } else if (comPick === 2) {
+        btnKertasCom.classList.add("changeColor");
+        playerWin.classList.remove("hide");
+    } else if (comPick === 3) {
+        btnGuntingCom.classList.add("changeColor");
+        draw.classList.remove("hide");
+    }
+});
+// btn kertas
+btnKertas.addEventListener("click", function(){
+    btnBatu.disabled = "true";
+    btnKertas.disabled = "true";
+    btnGunting.disabled = "true";
+    btnKertas.classList.add("changeColor");
+    versusAnc.classList.add("hide");
+    const comPick = Math.floor(Math.random() * 3)+1;
+    console.log(comPick)
+    if (comPick === 1) {
+        btnBatuCom.classList.add("changeColor");
+        playerWin.classList.remove("hide");
+    } else if (comPick === 2) {
+        btnKertasCom.classList.add("changeColor");
+        draw.classList.remove("hide");
+    } else if (comPick === 3) {
+        btnGuntingCom.classList.add("changeColor");
+        comWin.classList.remove("hide");
+    }
+});
+
+// a really lazy and desperate way to do refresh button
+btnRefresh.addEventListener("click", function(){
+    btnBatu.classList.remove("changeColor");
+    btnKertas.classList.remove("changeColor");
+    btnGunting.classList.remove("changeColor");
+    btnBatuCom.classList.remove("changeColor");
+    btnKertasCom.classList.remove("changeColor");
+    btnGuntingCom.classList.remove("changeColor");
+    versusAnc.classList.remove("hide");
+    playerWin.classList.add("hide");
+    comWin.classList.add("hide");
+    draw.classList.add("hide");
+})
